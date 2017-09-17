7e6468e1879db9d4afaad9dbf01308a0a260907c
Akash Agarwal <akash.software.1982@gmail.com>
2017-09-17 05:42:20 -0400

    Added Swagger and  Two API to get okta user.

---
+++ OktaWebAPI/Controllers/OktaController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Okta.Core.Clients;
+
+namespace OktaWebAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class OktaController : Controller
+    {
+        //This APi key name in OKTA is "oktaWebApi"
+        private readonly string token = "00k2Ltuw8RWyzAlRcgwyzLhf3L_1oww1VQ93hBOM2u";
+        private readonly string domain = "https://dev-409691.oktapreview.com";
+
+        // GET: api/Okta
+        [HttpGet("getallusers")]
+        public IList<string> GetAllUsers()
+        {
+            //Get an user based on the username  from okta server
+            var usersClient = new UsersClient(token, new Uri(domain));
+            IList<string> userNameList = new List<string>();
+
+            foreach (var usr in usersClient)
+            {
+                userNameList.Add(usr.Profile.FirstName);
+            }
+
+            return userNameList;
+        }
+
+        [HttpGet("getuser")]
+        public Okta.Core.Models.User GetUser()
+        {
+
+            //var usersClient1 = new UsersClient(token, new Uri(domain));
+            //var rr = usersClient1.GetByUsername("testuser@testuser.com");
+            //Get an user based on the username  from okta server
+            var oktaClient = new OktaClient(token, new Uri(domain));
+            var usersClient = oktaClient.GetUsersClient();
+            var user = usersClient.GetByUsername("testuser@testuser.com");
+            return user;
+        }
+
+        // POST: api/Okta
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            var oktaClient = new OktaClient(token, new Uri(domain));
+
+        }
+
+        // PUT: api/Okta/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- OktaWebAPI/Startup.cs
+++ OktaWebAPI/Startup.cs
@@ -23,12 +23,25 @@ namespace OktaWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                
+                c.SwaggerDoc("v1", 
+                    new Swashbuckle.AspNetCore.Swagger.Info
+                    { Title = "Okta API", Version = "v1", Description="Web Api ( core2) to consume okta API for major Okta workflows", 
+                        Contact = new Swashbuckle.AspNetCore.Swagger.Contact { Name="Rakesh Agarwal", Email= "rakesh.agarwal@capgemini.com", Url= "www.capgemini.com" } });
+                        });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger(c => c.PreSerializeFilters.Add((swagger, httpReq) => swagger.Host = httpReq.Host.Value));
+            app.UseSwaggerUI(t => {
+                t.SwaggerEndpoint("/swagger/v1/swagger.json", "This is Okta web api endPoints");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
